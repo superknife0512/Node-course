af9321c215aacbb77922c8d3f7405e948a18f0cf
Toan Phan <Toan Phan>
2018-11-09 11:16:35 +0700

    refactor code on product file

--- models/Products.js
+++ models/Products.js
@@ -5,6 +5,17 @@ const Cart = require('./Cart');
 
 const p = path.join(__dirname, '..', 'data', 'productData.json');
 
+function getDataFromFile(cb) {
+    fs.readFile(p, (err, fileContent) => {
+        if (!err) {
+            const returnData = JSON.parse(fileContent);
+            cb(returnData)
+        } else {
+            cb(null);
+        }
+    })
+}
+
 module.exports = class Product {
     constructor(id, title, imageUrl, price, des) {
         this.title = title;
@@ -15,29 +26,26 @@ module.exports = class Product {
     }
 
     addProduct() {
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) { //file exist - fetch all products from file
-                products = JSON.parse(fileContent);
 
-                if (this.id) {
-                    // check if it have ID that's time we update our product 
-                    const productIndex = products.findIndex(ele => ele.id === this.id);
-                    const updatedProducs = [...products];
-                    updatedProducs[productIndex] = this;
-                    fs.writeFile(p, JSON.stringify(updatedProducs), err => {
-                        console.log(err);
-                    })
-                } else {
-                    //we will create new product if don't have any ID yet
-                    this.id = Math.random().toString(35).substr(2, 7);
-                    products.push(this);
-                    fs.writeFile(p, JSON.stringify(products), err => {
-                        console.log(err);
-                    })
-                }
-                this.seoTitle();
+        getDataFromFile(products => {
+
+            if (this.id) {
+                // check if it have ID that's time we update our product 
+                const productIndex = products.findIndex(ele => ele.id === this.id);
+                const updatedProducs = [...products];
+                updatedProducs[productIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProducs), err => {
+                    console.log(err);
+                })
+            } else {
+                //we will create new product if don't have any ID yet
+                this.id = Math.random().toString(35).substr(2, 7);
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), err => {
+                    console.log(err);
+                })
             }
+            this.seoTitle();
         })
     }
 
@@ -47,33 +55,25 @@ module.exports = class Product {
     }
 
     static getProducts(fn) {
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                // file doesn't exist yet
-                console.log('get product fail' + err);
-                fn([])
-            } else {
-                fn(JSON.parse(fileContent))
-            }
+        getDataFromFile(products => {
+            fn(products)
         })
     }
 
     static getProductDetail(id, cb) {
-        fs.readFile(p, (err, data) => {
-            if (err) {
-                cb([])
-            } else {
-                const allProducts = JSON.parse(data);
-                const dataDetail = allProducts.find(ele => ele.id === id);
+        getDataFromFile(products => {
+            if (products) {
+                const dataDetail = products.find(ele => ele.id === id);
                 cb(dataDetail);
+            } else {
+                cb([])
             }
         })
     }
 
     static deleteProduct(id) {
-        fs.readFile(p, (err, fileContent) => {
-            // fetch data from products file
-            const products = JSON.parse(fileContent);
+
+        getDataFromFile(products => {
             const productDel = products.find(ele => ele.id === id)
             const productIndex = products.findIndex(ele => ele.id === id);
             products.splice(productIndex, 1);
